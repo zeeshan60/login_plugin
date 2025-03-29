42fb31cacb1d265ca8ea7dab42d72d4bf2b7fba7
zeeshantufail <zeeshan.tufail@aboitiz.com>
2025-03-29 22:18:55 +0800

    did some minor change

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -4,7 +4,7 @@ window.testEcho = () => {
     const inputValue = document.getElementById("echoInput").value;
     LoginPlugin.echo({value: inputValue}).then(result => {
             console.log(result.value);
-            document.getElementById("echoInput").value = result.value;
+            document.getElementById("echoInput").value = result.value + " ";
             document.getElementById("description").textContent = result.value;
         }
     );
